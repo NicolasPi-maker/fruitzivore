e6c3c9af16f7422b33ecb519ec22e3def455a7b2
eryan <eryan.gauvrit@gmail.com>
2023-09-27 13:42:20 +0200

    add header and his style

--- frontfruitzivore/src/components/Header/index.jsx
+++ frontfruitzivore/src/components/Header/index.jsx
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 function Header() {
+
+  const basketIcon = <i className="fa-solid fa-basket-shopping"></i>;
+
     return (
       <header>
-        header
+        <Link to='/' className="logo-bloc">
+          <img src={logo} alt='Fruitzivore' />
+        </Link>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/menu'>Menu</Link>
+          <Link to='/unit'>A la carte</Link>
+          <Link to='/contact'>{basketIcon}</Link>
+        </nav>
       </header>
     );
   }
--- frontfruitzivore/src/pages/Home/index.jsx
+++ frontfruitzivore/src/pages/Home/index.jsx
@@ -2,7 +2,7 @@
 function Home() {
   return (
     <main>
-      Main
+      
     </main>
   );
 }

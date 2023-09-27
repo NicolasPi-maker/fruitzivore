9688fc19b15bdd37040254fd46a626b2cb80f776
Pinier-Nicolas <pinier74nicolas@gmail.com>
2023-09-27 15:49:11 +0200

    Add emoji 2 🥳

--- backfruitzivore/routes/index.js
+++ backfruitzivore/routes/index.js
@@ -21,10 +21,23 @@ console.log('🥳');
 /* GET menu informations by id */
 router.get('/:id', function(req, res, next) {
   const currentId = req.params.id;
-  let getCurrentMenuQuery = `SELECT * FROM menus WHERE id =${currentId}`;
   if(!currentId) {
     res.status(404);
     throw new Error('Aucun identifiant n\'a été renseigné');
+  }
+  try {
+    let query = `
+      SELECT * FROM menus
+      JOIN plates p ON p.menu_id = ${currentId}
+    `;
+    sqlQuery(query, (error, results) => {
+
+      res.json(results);
+      res.status(200);
+    })
+  } catch (error) {
+    res.status(500);
+    throw new Error('Une erreur serveur est survenue');
   }
 });
 

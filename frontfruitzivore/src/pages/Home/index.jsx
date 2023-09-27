a68094d2a090af9f35cf4b9c9d154bd5c0b77aea
eryan <eryan.gauvrit@gmail.com>
2023-09-27 15:36:51 +0200

    fix button

--- frontfruitzivore/src/pages/Home/index.jsx
+++ frontfruitzivore/src/pages/Home/index.jsx
@@ -9,14 +9,14 @@ function Home({ data }) {
     setMenuHype(menusData[0]);
   }, [menusData])
   return (
-    <main>
+    <main className="main-home">
       <h1>Fruitzivore</h1>
       <p>LE site de commande en ligne à destination pour des fructivores</p>
       <div>
         <h2>Le menu en vogue</h2>
 
       </div>
-      <div>
+      <div className="list-row">
         <ButtonLink url='/menu' content='Nos menus' />
         <ButtonLink url='/unit' content='A la carte' />
       </div>
